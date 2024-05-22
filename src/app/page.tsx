2130007509981cf36688cf6890f99d4844759f22
Saad A. Bazaz <saadbazaz@hotmail.com>
2024-05-22 14:50:51 +0500

    fix(#1): fix build by forcing dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import CreateRoomButton from "./components/create-room-button";
 import JoinRoomButton from "./components/join-room-button";
 
 export default function Home() {
-
-  
-
-
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
